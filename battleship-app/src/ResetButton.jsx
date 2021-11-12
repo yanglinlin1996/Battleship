3e223d14de433336ab631a7c7b1c926b8b35d9a1
Linlin Yang <linlin@linlins-mbp.domain>
2021-11-12 12:05:22 -0800

    Add ResetButton

---
+++ battleship-app/src/ResetButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+export default function ResetButton(props) {
+    const {text} = props;
+
+    const dispatch = useDispatch();
+
+    return (
+        <button onClick = {
+            () => dispatch({
+                type: "REST",
+            })
+        }>
+            {text}
+        </button>
+    );
+}
